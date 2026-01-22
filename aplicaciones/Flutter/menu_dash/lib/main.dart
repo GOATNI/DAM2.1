3fc33ce4de9373afaf6d777923adcbfa845c31ba
GOATNI <mmuhammads@iesch.org>
2026-01-22 19:26:00 +0100

    Subida limpia sin archivos pesados

--- aplicaciones/Flutter/menu_dash/lib/main.dart
+++ aplicaciones/Flutter/menu_dash/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:menu_dash/screens/apijson_screen.dart';
+import 'package:menu_dash/screens/baloncesto_screen_1.dart';
 import 'package:menu_dash/screens/menu_screen.dart';
+<<<<<<< HEAD
 import 'package:menu_dash/screens/simpsons_screen.dart';
+=======
+import 'package:menu_dash/screens/perrosapi_scree.dart';
+>>>>>>> 9b3ff556f9d4d2b29f2de02cbdd65c15a9d1fb4c
 
 void main() => runApp(const MyApp());
 
@@ -15,7 +20,12 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => MenuScreen(),
         'api1': (context) => ApiJsonPlaceUsersScreen(),
+<<<<<<< HEAD
         'simpsons': (context) => SimpsonsScreen(),
+=======
+        'baloncesto': (context) => BaloncestoScreen1(),
+        'practica10' : (context) => PerrosRazaView(),
+>>>>>>> 9b3ff556f9d4d2b29f2de02cbdd65c15a9d1fb4c
       },
     );
   }
--- aplicaciones/Flutter/menudash2/android/app/src/main/kotlin/com/example/menu_dash/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.menu_dash
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
